0d4704dbfd2756d6e79a1883dec5de89bd55e872
the-solipsist <the.solipsist@gmail.com>
2020-03-19 04:01:55 +0530

    Update sortgs.py

--- sortgs.py
+++ sortgs.py
@@ -126,9 +126,9 @@ def setup_driver():
     print('Loading...')
     chrome_options = Options()
     chrome_options.add_argument("disable-infobars")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("disable-dev-shm-usage")
+    chrome_options.add_argument("headless")
+    chrome_options.add_argument("sortno-sandbox")
     driver = webdriver.Chrome(chrome_options=chrome_options)
     return driver
 
